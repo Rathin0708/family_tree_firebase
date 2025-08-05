b3e6d3d1315d07453978053118668e25383f3349
rathin <rathin007008@gmail.com>
2025-08-05 10:12:18 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Family_tree());
+}
+class Family_tree extends StatelessWidget {
+  const Family_tree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
